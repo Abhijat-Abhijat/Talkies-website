3f1af9cf331bbcabd60a95c1c97787234923e39b
ghostx31 <himanshu312001@gmail.com>
2021-10-17 12:45:56 +0530

    Changed .gitignore

--- talkies/talkies/settings.py
+++ talkies/talkies/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'talkies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'movied',
     }
 }
 
