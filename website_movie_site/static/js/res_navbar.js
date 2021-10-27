65bb213061d95771f0ddce447462d4ca953d4a21
ghostx31 <himanshu312001@gmail.com>
2021-10-27 12:23:34 +0530

    Removed a folder

--- website_movie_site/static/js/res_navbar.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "right_nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "right_nav";
-    }
-  }
